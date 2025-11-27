40b27f863f60e058fc23d217001446c58861815a
Bruno Rodrigues <fidbruno@hotmail.com>
2025-11-27 10:18:16 -0300

    add sua lista component

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,35 +1,22 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
-import Tag from './Tag.vue';
+import SuaLista from './SuaLista.vue';
 export default {
   data() {
     return {
-      ingredientes: ['Tomate', 'Cebola', 'Alface', 'Queijo'],
+      ingredientes: ['Alho', 'Manteiga', 'Orégano']
     };
   },
   components: {
     SelecionarIngredientes,
-    Tag,
+    SuaLista,
   },
 };
 </script>
 
 <template>
   <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto">
-        Sua lista:
-      </span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente"><Tag ativa :texto="ingrediente" /></li> 
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img src="../assets/images/icones/lista-vazia.svg" alt="icone de pesquisa">
-        Sua lista está vazia. Adicione ingredientes para começar a buscar receitas!
-      </p>
-    </section>
+    <SuaLista :ingredientes="ingredientes" />
 
     <SelecionarIngredientes />
   </main>
@@ -48,31 +35,6 @@ export default {
   gap: 5rem;
 }
 
-.sua-lista-texto {
-  color: var(--coral, #F0633C);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #F0633C);
-  text-align: center;
-}
 
 @media only screen and (max-width: 1300px) {
   .conteudo-principal {
---
+++ src/components/SuaLista.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+
+export default {
+  components: {
+    Tag,
+  },
+  props: {
+    ingredientes: { type: Array as PropType<string[]>, required: true },
+  }
+}
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto">
+      Sua lista:
+    </span>
+
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+      <li v-for="ingrediente in ingredientes" :key="ingrediente">
+        <Tag ativa :texto="ingrediente" />
+      </li>
+    </ul>
+
+    <p v-else class="paragrafo lista-vazia">
+      <img src="../assets/images/icones/lista-vazia.svg" alt="icone de pesquisa">
+      Sua lista está vazia. Adicione ingredientes para começar a buscar receitas!
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+</style>
